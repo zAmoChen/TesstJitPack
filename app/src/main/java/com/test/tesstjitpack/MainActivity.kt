09765d3a8dd7aafad10ffecfd6e6a385b7d37c05
dan.chen <z_am01o4d@163.com>
2021-10-14 15:30:04 +0800

    去掉版本号

--- app/src/main/java/com/test/tesstjitpack/MainActivity.kt
+++ app/src/main/java/com/test/tesstjitpack/MainActivity.kt
@@ -2,6 +2,10 @@ package com.test.tesstjitpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.fastyotest.library.YoTestCaptcha
+import com.fastyotest.library.YoTestCaptchaVerify
 import com.test.library.AlertUtils
 
 class MainActivity : AppCompatActivity() {
